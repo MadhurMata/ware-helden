6cff787b7a594a79007e60b0302f172b2d342abe
Madhur Suria Mata Garcia <madhurmata@gmail.com>
2020-03-23 20:33:50 +0000

    card-dimensions ans design

--- src/components/blogg-card/BloggCard.js
+++ src/components/blogg-card/BloggCard.js
@@ -1,22 +1,26 @@
 import React, { Component } from 'react';
 
-import {
-  Card, Button, CardImg, CardTitle, CardText,
-  CardSubtitle, CardBody
-} from 'reactstrap';
+import './BloggCard.css';
 
 export default class BloggCard  extends Component {
 
   render() {
     const { cardImg, cardText, cardTitle } = this.props; 
     return ( 
-      <Card>
-          <CardImg top width="100%" src={cardImg} alt="Card image cap" />
-          <CardBody>
-            <CardTitle>{cardTitle}</CardTitle>
-            <CardText>{cardText}</CardText>
-          </CardBody>
-        </Card>
+      <div id="card">
+        <div id="img">
+          <img src={cardImg} alt="Card image"/>
+        </div>
+        <div id="title">
+          {cardTitle}
+        </div>
+        <div id="text">
+          {cardText}
+        </div>
+        <div id="link">
+          <a href="/#">Read more...</a>
+        </div>
+      </div>
      );
   };
 
--- src/containers/blogg-list/BloggList.js
+++ src/containers/blogg-list/BloggList.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
 import BloggCard from '../../components/blogg-card/BloggCard';
 
-import { CardDeck } from 'reactstrap';
+import './BloggList.css';
+
 
 export default class BloggList  extends Component {
   render() {
     const cards = [
       {title:"madhur", cardText: "kjcbndjkchnedso", cardImg: "/assets/hola.png"}, 
+      {title:"WHAT IF YOUR FEARS SERVE A PURPOSE?", cardText: "Recently I felt fear in certain ways. A fear I recognize and am used to feeling sometimes, but this time it took a bit more of my positive energy away and somehow frustrated me a lot. I didn't want to feel it. I was disappointed in myself: ‘Really? Why do you always come back to me? Why can’t you just stay away? Will I ever be ‘strong’ enough to keep you out of my life? Will I ever be good enough? I mean, I’m only good enough when I’m without you right?’", cardImg: "/assets/hola.png"},
       {title:"esmee", cardText: "kjcbndjkchnedso", cardImg: "/assets/hola.png"}, 
       {title:"esmee", cardText: "kjcbndjkchnedso", cardImg: "/assets/hola.png"}, 
       {title:"esmee", cardText: "kjcbndjkchnedso", cardImg: "/assets/hola.png"}, 
-      {title:"esmee", cardText: "kjcbndjkchnedso", cardImg: "/assets/hola.png"}, 
-      {title:"esmee", cardText: "kjcbndjkchnedso", cardImg: "/assets/hola.png"}, 
-      {title:"iris", cardText: "kjcbndjkchnedso", cardImg: "/assets/hola.png"}];
+      {title:"esmee", cardText: "kjcbndjkchnedso", cardImg: "/assets/hola.png"} 
+    ];
     return (
       <div class="container">
-      <CardDeck>
         {cards.map((card, index) => 
         <BloggCard
           key={index}
@@ -24,7 +24,6 @@ export default class BloggList  extends Component {
           cardText={card.cardText}
         />
           )}
-      </CardDeck>
       </div> 
     );
   }
